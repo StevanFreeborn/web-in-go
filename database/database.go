dd21a2e67fdf4907894aa767fab405b3bd994827
Stevan Freeborn <65925598+StevanFreeborn@users.noreply.github.com>
2025-03-26 22:07:46 -0500

    feat: figure out go + sqlite

--- database/database.go
+++ database/database.go
@@ -3,6 +3,7 @@ package main
 import (
 	"database/sql"
 	"fmt"
+	"time"
 
 	_ "github.com/mattn/go-sqlite3"
 )
@@ -12,13 +13,102 @@ func main() {
 	defer db.Close()
 
 	if openErr != nil {
-		fmt.Print(openErr)
+		fmt.Println(openErr)
 	}
 
 	connectionErr := db.Ping()
 
 	if connectionErr != nil {
-		fmt.Print(connectionErr)
+		fmt.Println(connectionErr)
 	}
 
+	createUsersTableQuery := `
+    CREATE TABLE users (
+      id INTEGER PRIMARY KEY AUTOINCREMENT,
+      username TEXT NOT NULL,
+      password TEXT NOT NULL,
+      created_at DATETIME
+    );
+  `
+
+	_, createUserTableError := db.Exec(createUsersTableQuery)
+
+	if createUserTableError != nil {
+		fmt.Println(createUserTableError)
+	}
+
+	newUserUsername := "john.wick"
+	newUserPassword := "supersecretpassword"
+	newUserCreatedAt := time.Now()
+
+	createUserQuery := `
+    INSERT INTO users (username, password, created_at) VALUES (?, ?, ?)
+  `
+
+	createUserResult, createUserErr := db.Exec(
+		createUserQuery,
+		newUserUsername,
+		newUserPassword,
+		newUserCreatedAt,
+	)
+
+	if createUserErr != nil {
+		fmt.Println(createUserErr)
+	}
+
+	userId, userIdErr := createUserResult.LastInsertId()
+
+	if userIdErr != nil {
+		fmt.Println(userIdErr)
+	}
+
+	fmt.Println(userId)
+
+	var username string
+
+	getUserQuery := "SELECT username FROM users WHERE id = ?"
+	getUserErr := db.QueryRow(getUserQuery, userId).Scan(&username)
+
+	if getUserErr != nil {
+		fmt.Println(getUserErr)
+	}
+
+	fmt.Println(username)
+
+	userRows, getUsersErr := db.Query("SELECT * FROM users")
+
+	if getUsersErr != nil {
+		fmt.Println(getUsersErr)
+	}
+
+	defer userRows.Close()
+
+	type user struct {
+		id        int
+		username  string
+		password  string
+		createdAt time.Time
+	}
+
+	var users []user
+
+	for userRows.Next() {
+		var u user
+
+		getUserErr := userRows.Scan(&u.id, &u.username, &u.password, &u.createdAt)
+
+		if getUserErr != nil {
+			fmt.Println(getUserErr)
+		}
+
+		users = append(users, u)
+	}
+
+	userRowsErr := userRows.Err()
+
+	if userRowsErr != nil {
+		fmt.Println(userRowsErr)
+	}
+
+	fmt.Printf("%#v", users)
 }
