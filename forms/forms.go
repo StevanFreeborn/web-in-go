4a0f77bdf2600100339b87ffe88e24746768d07e
Stevan Freeborn <65925598+StevanFreeborn@users.noreply.github.com>
2025-03-29 15:52:01 -0500

    feat: learn about forms

---
+++ forms/forms.go
@@ -0,0 +1,40 @@
+package main
+
+import (
+	"fmt"
+	"html/template"
+	"net/http"
+)
+
+type ContactDetails struct {
+	Email   string
+	Subject string
+	Message string
+}
+
+func main() {
+	tmpl := template.Must(template.ParseFiles("forms.html"))
+
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		if r.Method != http.MethodPost {
+			tmpl.Execute(w, nil)
+			return
+		}
+
+		details := ContactDetails{
+			Email:   r.FormValue("email"),
+			Subject: r.FormValue("subject"),
+			Message: r.FormValue("message"),
+		}
+
+		fmt.Println(details.Email)
+
+		_ = details
+
+		tmpl.Execute(w, struct{ Success bool }{true})
+	})
+
+	fmt.Println("Server listening on port 8080")
+
+	http.ListenAndServe(":8080", nil)
+}
---
+++ forms/go.mod
@@ -0,0 +1,3 @@
+module web-in-go/forms
+
+go 1.24.1
